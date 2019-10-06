9def1f4a14eee2ee98794fd3d8b35a7ea1f7006c
Nadi <nadi@MacBook-Pro-Nadi.local>
2019-10-06 18:37:44 +0300

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Header.css';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}> 
+                <img src='https://habrastorage.org/getpro/moikrug/uploads/user/100/014/278/9/avatar/medium_134166cbbb3aa78cb0865b8c0dff70e2.jpg' alt='logo' />
+            </div>
+        </header>
+    )
+}
+
+export default Header;
